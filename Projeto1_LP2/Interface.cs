ee5d0ffec7b758dcf52a23aac85302077be62126
FiammaV <ines.perdigao.nunes@gmail.com>
2018-11-24 12:29:59 +0000

    Continuei o ShowMenu e adicionei o ShowInfo

--- Projeto1_LP2/Interface.cs
+++ Projeto1_LP2/Interface.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Projeto1_LP2
 {
-    class Interface
+    public class Interface : InterfaceManager
     {
+        public void ShowMenu()
+        {
+            Console.WriteLine("1. Show game's info\n" +
+                              "2. Make a research\n" +
+                              "3. Leave\n");
+
+            ReadEntry();
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Insert the game's ID, please. \n");
+
+            Info(id);
+        }
+
     }
 }
