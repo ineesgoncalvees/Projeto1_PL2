5043e50b3a05ea60bd917771a7d06483db1f5010
FiammaV <ines.perdigao.nunes@gmail.com>
2018-11-24 12:29:18 +0000

    Adicionei o método ReadEntry

---
+++ Projeto1_LP2/InterfaceManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto1_LP2
+{
+    public class InterfaceManager
+    {
+
+        public int ReadEntry()
+        {
+            int read_number;
+
+            while (true)
+            {
+                read_number = Console.Read();
+
+                switch (read_number)
+                {
+                    case '1':
+                        Info(id);
+                        break;
+
+                    case '2':
+                        Search();
+                        break;
+
+                    case '3':
+                        return read_number;
+                }
+            }
+        }
+        public void Info(int id)
+        {
+            
+        }
+
+        private void Search()
+        {
+            int read_id;
+            read_id = Console.Read();
+        }
+    }
+}
